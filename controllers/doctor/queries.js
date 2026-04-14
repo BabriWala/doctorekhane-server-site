f7d0a75ad5a4c5e37a2950f4b6a816a5a0eae58a
BabriWala <hanzalafarhann@gmail.com>
2026-04-14 10:05:36 +0600

    queries update by order and status

--- controllers/doctor/queries.js
+++ controllers/doctor/queries.js
@@ -5,15 +5,16 @@ const Doctor = require("../../models/Doctor");
 // ======================================
 const getAllDoctors = async (req, res) => {
   try {
-    const { status, department, field, search } = req.query;
+    const { department, field, search } = req.query;
 
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
-    let filter = {};
+    let filter = {
+      "professional.status": "Active",
+    };
 
-    if (status) filter["professional.status"] = status;
     if (department) filter["professional.department"] = department;
     if (field) filter["professional.field"] = field;
 
@@ -32,9 +33,12 @@ const getAllDoctors = async (req, res) => {
     const totalItems = await Doctor.countDocuments(filter);
 
     const doctors = await Doctor.find(filter)
+      .sort({
+        "professional.order": 1, // 👈 main feature
+        createdAt: -1,
+      })
       .skip(skip)
-      .limit(limit)
-      .sort({ createdAt: -1 });
+      .limit(limit);
 
     return res.status(200).json({
       success: true,
